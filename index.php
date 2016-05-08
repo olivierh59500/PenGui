5bcedc1668ec95e80a421d77d0410afd699b08fe
Mo <mojtabaamiri93@gmail.com>
2016-05-08 03:17:11 +0100

    Home Page
    Added information to the home page

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['loginUser'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PenGui Admin</title>
+    <title>Welcome to PenGui</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/css/bootstrap.min.css" rel="stylesheet">
@@ -119,10 +119,45 @@ if(!isset($_SESSION['loginUser'])) {
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Welcome to PenGui
+                            Welcome to PenGui! - <b><i>BETA!</i></b>
                         </h1>
-                        <h4 align="center">Start your vulnerability scanning now!</h4>
-                        <p>This website is created for </p>
+                        <h2 align="center">Start your vulnerability scanning now!</h2>
+                        <p>This website is created for people who are interested in security and in security of their
+                            infrastructure.The aim of PenGui is to provide a one stop shop for most users looking to
+                            scan their servers and services to reveal any vulnerabilities found by PenGui using open
+                            source security tools.</p>
+                        For example:
+                        <ul>
+                            <li>Scan your network and servers from external IP address</li>
+                            <li>Save your self time and the hassle form installing these tools on your local machine
+                            </li>
+                            <li>Some scans could be blocked or filtered by your network</li>
+                            <li>Ease of access and human readable logs</li>
+                            <li>Each scan type has information about how they work</li>
+                        </ul>
+                        <br>
+                        <h4>Getting Started</h4>
+                        <ul>
+                            <li>All the tools are available on the left hand side.</li>
+                            <li>Each of these tools are clearly presented with information on how to use them.</li>
+                            <li>After a successful scan you will find your scan results in <b>My Scans</b></li>
+                        </ul>
+                        <h4>Things to still come:</h4>
+                        <ul>
+                            <li>Further Log Simplification - Easier to read Logs returned via the tools</li>
+                            <li>Shed more features</li>
+                        </ul>
+                        <h4>Bugs bugs bugs!</h4>
+                        <p>If you have identified a bug please report it at: bugs@pengui.uk</p>
+                        <p>For feedback and sites issues please contact webmaster@pengui.uk</p>
+                        <br><br><br>
+                        <div align="center">
+                            <div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert"
+                                                               aria-label="close">&times;</a>
+                                <strong>WARNING: YOU MUST HAVE PERMISSIONS BEFORE TARGETING A HOST USING THE
+                                    TOOLS PROVIDED ON PenGui</strong>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
